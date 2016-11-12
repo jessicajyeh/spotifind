505cc85d5b46cc798db576adeab95f3a10970d86
thedanielzhang <daniel.zhang@berkeley.edu>
2016-11-12 10:43:51 -0800

    committing changes
    temp temp

--- spotifind/app.js
+++ spotifind/app.js
@@ -5,10 +5,18 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
+
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+var mongoose = require('mongoose');
+require('./models/Locations');
+mongoose.connect('mongodb://localhost/locations');
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- spotifind/routes/index.js
+++ spotifind/routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+var Location = mongoose.model('Location');
+
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/locations', function(req, res, next) {
+  Location.find(function(err, locations){
+  	if(err) { return next(err); }
+
+  	res.json(locations);
+  });
+});
+
+/* POST */
+router.post('/locations', function(req, res, next) {
+	var location = new Location(req.body);
+
+	location.save(function(err, location){
+		if(err){ return next(err); }
+
+		res.json(location);
+
+	});
+	
+
 });
 
 module.exports = router;
