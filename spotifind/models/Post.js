200bc6ca10278e49a90630b16bf211007745a175
Divya Vijayan <vijayan.pablomantis@gmail.com>
2016-11-12 09:10:35 -0800

    factory_code

---
+++ spotifind/models/Post.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+
+var PostSchema = new mongoose.PostSchema({
+	title: String,
+	link: String,
+	upvotes: {type: Number, default: 0},
+	comments:[{type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
+
+});
+
+mongoose.model('Post', PostSchema);
+
+
+
+
+
--- spotifind/public/javascripts/angularApp.js
+++ spotifind/public/javascripts/angularApp.js
@@ -1,15 +1,25 @@
 var app = angular.module('spotifind', []);
 
+
+app.factory('posts', [function(){
+  var o = {
+  	posts: []
+  };
+  return o;
+}]);
+
 app.controller('MainCtrl', [
 	'$scope',
+	'posts',
 
 	//$scope.latitude,
 	//$scope.longitude,
 
-	function($scope){
+	function($scope, posts){
 		$scope.latitude = '0';
 		$scope.longitude = '0';
 		$scope.test = 'Hello World!';
+		$scope.posts = posts.posts;
 		$scope.location_array = [];
 		//var x = document.getElementById("demo");
 		$scope.getLocation = function(){
@@ -30,6 +40,7 @@ app.controller('MainCtrl', [
 			 $scope.location_array.push([$scope.latitude, $scope.longitude]);
 		};
 	}
+
 
 
 
